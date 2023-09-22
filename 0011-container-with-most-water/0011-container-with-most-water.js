1dd79e69fd7228ef1f7eb429612b07f268d7ac4f
NayeongK <80331804+NayeongK@users.noreply.github.com>
2023-09-22 14:49:17 +0900

    Time: 57 ms (90.87%), Space: 49.3 MB (77.03%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -5,12 +5,12 @@
 const maxArea = function(height) {
   let left = 0;
   let right = height.length - 1;
-  let max_area = 0;
+  let maxArea = 0;
 
   while (left < right) {
     const currentHeight = Math.min(height[left], height[right]);
     const currentWidth = right - left;
-    max_area = Math.max(max_area, currentHeight * currentWidth);
+    maxArea = Math.max(maxArea, currentHeight * currentWidth);
 
     if (height[left] < height[right]) {
       left++;
@@ -19,5 +19,5 @@ const maxArea = function(height) {
     }
   }
 
-  return max_area;
+  return maxArea;
 };
