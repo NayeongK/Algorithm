edd9a8bb837368548e43e116191e555093d48757
NayeongK <80331804+NayeongK@users.noreply.github.com>
2023-10-04 14:14:52 +0900

    Time: 56 ms (27.22%), Space: 42.4 MB (26.75%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -5,8 +5,12 @@
  */
 const searchInsert = function(nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) return i;
-    if (target < nums[i]) return i;
+    if (nums[i] === target) {
+      return i
+    };
+    if (target < nums[i]) {
+      return i
+    };
   }
   
   return nums.length;
