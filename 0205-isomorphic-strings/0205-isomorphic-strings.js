6a965fb22d150a32df5639797de3a94067acbc5c
NayeongK <80331804+NayeongK@users.noreply.github.com>
2023-10-11 13:26:37 +0900

    Time: 57 ms (81.42%), Space: 41.7 MB (97.67%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isIsomorphic = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) !== t.indexOf(t[i])) {
+      return false;
+    }
+  }
+  
+  return true;
+};
