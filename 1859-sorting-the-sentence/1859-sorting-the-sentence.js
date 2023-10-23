72afdd0f58635c6440c0d0d29eeeba926048140f
NayeongK <80331804+NayeongK@users.noreply.github.com>
2023-10-23 23:14:44 +0900

    Time: 46 ms (81.74%), Space: 41.4 MB (89.45%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.js
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -3,8 +3,8 @@
  * @return {string}
  */
 const sortSentence = function(s) {
-  const words = s.split(' ');
+  const words = s.split(" ");
   words.sort((a, b) => a[a.length - 1] - b[b.length - 1]);
 
-  return words.map(word => word.slice(0, -1)).join(' ');
+  return words.map(word => word.slice(0, -1)).join(" ");
 };
