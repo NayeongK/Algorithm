c5148b4874341ee649b8daee32bfc246f7b70460
NayeongK <80331804+NayeongK@users.noreply.github.com>
2023-11-06 13:36:28 +0900

    Time: 54 ms (58.13%), Space: 42.7 MB (34.17%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+const numJewelsInStones = function(jewels, stones) {
+  let count = 0;
+  for (let i = 0; i < stones.length; i++) {
+    let currentStone = stones[i]
+    for (let j = 0; j < jewels.length; j++) {
+      let currentJewel = jewels[j]
+      
+      if (currentStone === currentJewel) {
+        count++;
+      }
+    }
+  }
+  
+  return count;
+};
