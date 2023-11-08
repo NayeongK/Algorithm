afeb70964f2e3c3199528983c69948d561a13238
NayeongK <80331804+NayeongK@users.noreply.github.com>
2023-11-08 15:29:19 +0900

    Time: 3487 ms (14.36%), Space: 51.1 MB (55.02%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -5,7 +5,6 @@
 const replaceElements = function (arr) {
   const result = [];
   const copiedArr = [...arr];
-  let GreatestNumber = null;
   for (let i = 0; i < arr.length - 1; i++) {
     copiedArr.shift();
     result.push(Math.max.apply(null, copiedArr));
