191dc1167c40ab837d3cc048ac93ff8e55791c4d
NayeongK <80331804+NayeongK@users.noreply.github.com>
2023-12-13 09:19:39 +0900

    Time: 82 ms (79.58%), Space: 55.8 MB (23.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,20 +3,19 @@
  * @return {number[]}
  */
 const productExceptSelf = function(nums) {
-  const length = nums.length;
   const output = [];
-
   let product = 1;
-  for (let i = 0; i < length; i++) {
+
+  for (let i = 0; i < nums.length; i++) {
     output[i] = product;
     product *= nums[i];
   }
-
+  
   product = 1;
-  for (let i = length - 1; i >= 0; i--) {
+  for (let i = nums.length -1; i >= 0; i--) {
     output[i] *= product;
     product *= nums[i];
   }
-
+  
   return output;
 };
