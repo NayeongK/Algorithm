03ad2e8bf53e792745c6168bf9466e7f464bd4ab
곽나영 <80331804+NayeongK@users.noreply.github.com>
2024-02-07 19:04:36 +0900

    Time: 109 ms (67.39%), Space: 64 MB (6.23%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+const groupAnagrams = function(strs) {
+  const obj = {};
+  
+  let sortedStr = null;
+  for (let i = 0; i< strs.length; i++) {
+    sortedStr = strs[i].split("").sort().join();
+    if (!obj[sortedStr]) {
+      obj[sortedStr] = []
+    }
+    obj[sortedStr].push(strs[i])
+  }
+  
+  return Object.values(obj);
+};
