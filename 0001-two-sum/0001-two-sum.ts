2af851e7e7a4184e9aa32963f67592f832985de2
곽나영 <80331804+NayeongK@users.noreply.github.com>
2024-05-19 00:59:33 -0700

    Time: 100 ms (38.30%), Space: 52.3 MB (59.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,8 +1,8 @@
 function twoSum(nums: number[], target: number): number[] {
   for (let i = 0; i < nums.length - 1; i++) {
     let diff = target - nums[i];
-    let pairIndex = nums.indexOf(diff, i+1);
-    if (pairIndex !== -1 && pairIndex !== i) {
+    let pairIndex = nums.indexOf(diff, i + 1);
+    if (pairIndex !== -1) {
       return [i, pairIndex];
     }
   }
